c4a22f41fe43bd25dee54d4210ae71f4ad8dd595
Ziad Manour <70920660+ziad-ma@users.noreply.github.com>
2020-09-19 03:54:52 +0300

    The final form
    the code is completly ready now

--- The_Code/code2/code2.ino
+++ The_Code/code2/code2.ino
@@ -1,8 +1,14 @@
 #include <TimerOne.h>
 int counter=0;
+int counter1=0;
+int counter2=0;
 #define SCR_PIN 3
+#define SCR_PIN1 5
+#define SCR_PIN2 6
 int max_counter=0;
 boolean zero_detect=false;
+boolean zero_detect1=false;
+boolean zero_detect2=false;
 void phase_shift_counter()
 {
   if(zero_detect&&counter>=max_counter)
@@ -12,6 +18,26 @@ void phase_shift_counter()
   }
   counter++;
 }
+
+void phase_shift_counter1()
+{
+  if(zero_detect1&&counter1>=max_counter)
+  {
+    digitalWrite(SCR_PIN1,HIGH);
+    zero_detect1=false;
+  }
+  counter1++;
+}
+
+void phase_shift_counter2()
+{
+  if(zero_detect2&&counter2>=max_counter)
+  {
+    digitalWrite(SCR_PIN2,HIGH);
+    zero_detect2=false;
+  }
+  counter2++;
+}
 void isr()
 {
   zero_detect=true;
@@ -19,11 +45,35 @@ void isr()
   digitalWrite(SCR_PIN,LOW);
 
 }
+
+void isr1()
+{
+  zero_detect1=true;
+  counter1=0;
+  digitalWrite(SCR_PIN1,LOW);
+
+}
+
+void isr2()
+{
+  zero_detect2=true;
+  counter2=0;
+  digitalWrite(SCR_PIN2,LOW);
+
+}
 void setup() {
 pinMode(SCR_PIN,OUTPUT);
+pinMode(SCR_PIN1,OUTPUT);
+pinMode(SCR_PIN2,OUTPUT);
 Timer1.initialize(10);
 Timer1.attachInterrupt(phase_shift_counter);
+Timer1.initialize(10);
+Timer1.attachInterrupt(phase_shift_counter1);
+Timer1.initialize(10);
+Timer1.attachInterrupt(phase_shift_counter2);
 attachInterrupt(0,isr,RISING);
+attachInterrupt(1,isr1,RISING);
+attachInterrupt(1,isr2,RISING);
 }
 
 void loop() {
